e18bf6139bf41a96f91a4efe00917dade2f0640e
Aleksandra Zhukova <a.hahina@gmail.com>
2020-11-04 21:13:59 +0300

    Main

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int price_of_ticket = 9999;
+        int bonus_miles = price_of_ticket / 20;
+        System.out.println(bonus_miles);
+    }
+}
